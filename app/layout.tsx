6f1c4ab8d0dece9a453b28052d9a98a514d29568
XBOfficiel <X_BOfficial@proton.me>
2025-10-16 16:16:02 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,8 @@ import { Analytics } from '@vercel/analytics/next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
+  title: 'SpicyMemes x Spiice 🌶️',
+  description: 'SpicyMemes x Spiice 🌶️',
   generator: 'v0.app',
 }
 
